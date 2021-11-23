f38c8653a3a2febf1f20a89c4d5d4cb324fece5b
Nightmare9254 <adrian.sarna59@gmail.com>
2021-11-23 16:02:54 +0100

    format string for api calls and dispalting

---
+++ src/helpers/stringFormats.ts
@@ -0,0 +1,11 @@
+import { SortTypes } from '../interfaces/repoInterfaces';
+
+export const formatDisplay = (string: SortTypes) =>
+  typeof string === 'string'
+    ? `${string[0].toUpperCase()}${string.slice(1).split('-').join(' ')}`
+    : string;
+
+export const formatGHApiUrl = (string: string, regexp?: RegExp) =>
+  string
+    .replace(/https:\/\/api.github.com\//, '')
+    .replace(regexp ? regexp : /(.*?)/, '');
