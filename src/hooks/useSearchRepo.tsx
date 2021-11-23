9ad8b741462fc879ef3019f823a6b4492b7589fe
Nightmare9254 <adrian.sarna59@gmail.com>
2021-11-23 16:03:25 +0100

    Context for searching new repos

---
+++ src/hooks/useSearchRepo.tsx
@@ -0,0 +1,55 @@
+import { createContext, FC, useContext, useReducer, useState } from 'react';
+import { IRepos, SortTypes } from '../interfaces/repoInterfaces';
+import { Actions, ActionType, State } from '../interfaces/searchReducer';
+
+const { SEARCH, SET_REPOS } = ActionType;
+
+const reducer = (repos: State<IRepos>, action: Actions) => {
+  switch (action.type) {
+    case SET_REPOS: {
+      return action.payload;
+    }
+    default:
+      throw new Error(`Unhandled action: ${action.type}`);
+  }
+};
+
+const useSearchReducer = () => {
+  const [repos, dispatch] = useReducer(reducer, {
+    incomplete_results: false,
+    total_count: 0,
+  });
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState<SortTypes>(SortTypes.BEST_MATCH);
+  return {
+    repos,
+    dispatch,
+    search,
+    setSearch,
+    sort,
+    setSort,
+  };
+};
+
+const SearchRepoContext = createContext<ReturnType<
+  typeof useSearchReducer
+> | null>(null);
+
+export const useRepoContext = () => {
+  const ctx = useContext(SearchRepoContext)!;
+
+  if (!ctx)
+    throw new Error(
+      'Context is undefined or you are useing it outside provider'
+    );
+
+  return ctx;
+};
+
+export const SearchRepoProvider: FC = ({ children }) => {
+  return (
+    <SearchRepoContext.Provider value={useSearchReducer()}>
+      {children}
+    </SearchRepoContext.Provider>
+  );
+};
---
+++ src/providers/RepoProvider.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { SearchRepoProvider } from '../hooks/useSearchRepo';
+
+const RepoProvider: FC = ({ children }) => {
+  return <SearchRepoProvider>{children}</SearchRepoProvider>;
+};
+
+export default RepoProvider;
